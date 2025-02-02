640bb26537e8382c7ab95dbbabc01fe0ab02d82e
Jusaca01 <jusaca01@alumnatflorida.es>
2025-02-02 23:32:46 +0100

    commit2

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Generador de Hola Mundo</title>
+    <title>Generador de Hola Mundo's</title>
     <style>
         body {
             font-family: Arial, sans-serif;
